f01efdc65a6ed8616e857d975cf2545d59bd40ad
David Mateos <damarev@gmail.com>
2016-03-07 19:45:48 +0100

    subida lunes 7/3

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,57 @@
 
 (function($) {
 
-    /**
-     * Redimensiona gráfico de columnas
-     * @param {Number[4]} pcts - Porcentajes de las 4 barras. Ej: [8,24,15,53]
-     */
-    $.fn.graphCols = function(pcts) {
-        setColPct(this, '.col-A', pcts[0]);
-        setColPct(this, '.col-B', pcts[1]);
-        setColPct(this, '.col-C', pcts[2]);
-        setColPct(this, '.col-D', pcts[3]);
+    var form, errors, is_form_ok;
+
+    $.fn.enableValidation = function() {
+        if(this.prop("tagName") != 'FORM'){
+            if(console && console.log) console.log("Element is not a FORM.");
+            return false;
+        }
+
+        form = this;
+        form.attr('novalidate', 'novalidate').on('submit', onSubmit);
+        errors = $('div.errors', form);
     }
 
-    function setColPct(context, selector, pct){
-        context.children(selector).css('width', pct+'%').find('.bar > span').contents().first().replaceWith(pct);
+    function onSubmit(e){
+        is_form_ok = true;
+        errors.html('');
+
+        $('*[data-validation]', form).removeClass('error').each(validateField);
+
+        if(!is_form_ok){
+            e.preventDefault();
+        }
     }
 
-    /**
-     * Setea tooltips en grafico coordenadas
-     * @param {Number[4]} values - Porcentajes de las 4 barras. Ej: [8,24,15,53]
-     */
-    $.fn.graphCoords = function(texts) {
-        setBoxTooltip(this, '.box-A', texts[0]);
-        setBoxTooltip(this, '.box-B', texts[1]);
-        setBoxTooltip(this, '.box-C', texts[2]);
-        setBoxTooltip(this, '.box-D', texts[3]);
+    function validateField(index, node){
+        var elem  = $(node);
+        var is_ok = isValidField(elem, elem.data('validation'));
+
+        if(!is_ok){
+            is_form_ok = false;
+            elem.addClass('error');
+            var errorMsg = $('<p>').text( elem.data('error-msg') )
+            errors.append(errorMsg)
+        }
     }
 
-    function setBoxTooltip(context, selector, contents){
-        context.find(selector).find('.tooltip-box').html(contents);
+    function isValidField(elem, rule){
+        if('not-empty' == rule){
+            return elem.val() != '';
+        }else if('email' == rule){
+            return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,6})+$/.test(elem.val());
+        }else if('repeat' == rule){
+            return elem.val() == $(elem.data('repeat-field')).val();
+        }else if('telephone' == rule){
+            return elem.val() != '';
+        }else if('select' == rule){
+            return elem.val() != '';
+        }else if('checkbox' == rule){
+            return elem.is(':checked');
+        }
+        return true;
     }
 
 }( jQuery ));
@@ -52,6 +75,7 @@ $(function () {
 
         $('.key-benefits .content > div').hide();
         $('.key-benefits .nav a').eq(0).trigger('click');
+
     }
 
 
@@ -64,10 +88,36 @@ $(function () {
             }else{
                 me.addClass('open').next().slideDown(150);
             }
-
         }).find('> div').hide();
 
     }
+
+
+    if( $('.news-list').length ){
+
+        var allItems  = $('.news-list .news-item');
+        var numItems  = allItems.length;
+        var maxItems  = $('.news-list').data('max-items');
+        var moreItems = $('.news-list').data('more-items');
+
+        if(numItems > maxItems){
+            allItems.filter(':gt('+(maxItems-1)+')').parent().hide();
+            $('.news-list .load-more').removeClass('hidden').on('click', 'span', function(){
+                var n = $('.news-list .news-item').filter(':hidden:lt('+moreItems+')').parent().slideDown(150);
+
+                if(allItems.filter(':hidden').length == 0){
+                    $('.news-list .load-more').addClass('hidden');
+                }
+            });
+        }
+
+    }
+
+
+    if( $('form[data-validate="true"]').length ){
+        $('form[data-validate="true"]').enableValidation();
+    }
+
 
 });
 
