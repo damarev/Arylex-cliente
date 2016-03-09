6e7079ee80d78759fd78be9b9b43d4854eae26d2
David Mateos <damarev@gmail.com>
2016-03-09 16:41:44 +0100

    responsive 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,65 +1,12 @@
 
-(function($) {
-
-    var form, errors, is_form_ok;
-
-    $.fn.enableValidation = function() {
-        if(this.prop("tagName") != 'FORM'){
-            if(console && console.log) console.log("Element is not a FORM.");
-            return false;
-        }
-
-        form = this;
-        form.attr('novalidate', 'novalidate').on('submit', onSubmit);
-        errors = $('div.errors', form);
-    }
-
-    function onSubmit(e){
-        is_form_ok = true;
-        errors.html('');
-
-        $('*[data-validation]', form).removeClass('error').each(validateField);
-
-        if(!is_form_ok){
-            e.preventDefault();
-        }
-    }
-
-    function validateField(index, node){
-        var elem  = $(node);
-        var is_ok = isValidField(elem, elem.data('validation'));
-
-        if(!is_ok){
-            is_form_ok = false;
-            elem.addClass('error');
-            var errorMsg = $('<p>').text( elem.data('error-msg') )
-            errors.append(errorMsg)
-        }
-    }
-
-    function isValidField(elem, rule){
-        if('not-empty' == rule){
-            return elem.val() != '';
-        }else if('email' == rule){
-            return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,6})+$/.test(elem.val());
-        }else if('repeat' == rule){
-            return elem.val() == $(elem.data('repeat-field')).val();
-        }else if('telephone' == rule){
-            return elem.val() != '';
-        }else if('select' == rule){
-            return elem.val() != '';
-        }else if('checkbox' == rule){
-            return elem.is(':checked');
-        }
-        return true;
-    }
-
-}( jQuery ));
-
-
-
-
 $(function () {
+
+
+    $('.header-nav .logo-top .menu-toggle').on('click', function(e){
+        e.preventDefault();
+        $(this).closest('.header-nav').toggleClass('mobile-open')
+    });
+
 
     if( $('.key-benefits').length ){
 
@@ -120,5 +67,64 @@ $(function () {
 
 
 });
+
+
+
+(function($) {
+
+    var form, errors, is_form_ok;
+
+    $.fn.enableValidation = function() {
+        if(this.prop("tagName") != 'FORM'){
+            if(console && console.log) console.log("Element is not a FORM.");
+            return false;
+        }
+
+        form = this;
+        form.attr('novalidate', 'novalidate').on('submit', onSubmit);
+        errors = $('div.errors', form);
+    }
+
+    function onSubmit(e){
+        is_form_ok = true;
+        errors.html('');
+
+        $('*[data-validation]', form).removeClass('error').each(validateField);
+
+        if(!is_form_ok){
+            e.preventDefault();
+        }
+    }
+
+    function validateField(index, node){
+        var elem  = $(node);
+        var is_ok = isValidField(elem, elem.data('validation'));
+
+        if(!is_ok){
+            is_form_ok = false;
+            elem.addClass('error');
+            var errorMsg = $('<p>').text( elem.data('error-msg') )
+            errors.append(errorMsg)
+        }
+    }
+
+    function isValidField(elem, rule){
+        if('not-empty' == rule){
+            return elem.val() != '';
+        }else if('email' == rule){
+            return /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,6})+$/.test(elem.val());
+        }else if('repeat' == rule){
+            return elem.val() == $(elem.data('repeat-field')).val();
+        }else if('telephone' == rule){
+            return elem.val() != '';
+        }else if('select' == rule){
+            return elem.val() != '';
+        }else if('checkbox' == rule){
+            return elem.is(':checked');
+        }
+        return true;
+    }
+
+}( jQuery ));
 
 
