e22cc7074f1ebfad7043381fe8992c37a2b8ab97
David Mateos <damarev@gmail.com>
2016-03-24 01:32:55 +0100

    css

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var sourcemaps     = require('gulp-sourcemaps');
 gulp.task('sass', function() {
     return gulp.src("app/scss/main.scss")
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
